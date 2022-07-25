609fc72550394bccd5712cf52a565e586349cbf2
RajGupta-1101 <85207552+RajGupta-1101@users.noreply.github.com>
2022-07-25 21:18:05 +0530

    Update

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,12 +2,14 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
+const SERVER_URL = process.env.SERVER_URL || 'http://127.0.0.1:3000';
+
 export const updateSettings = async (data, type) => {
   try {
     const url =
       type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+        ? `${SERVER_URL}/api/v1/users/updateMyPasswor`
+        : `${SERVER_URL}/api/v1/users/updateMe`;
 
     const res = await axios({
       method: 'PATCH',
